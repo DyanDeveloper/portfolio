a5d672e8310bf28575e69c4848d354c9492e498d
DyanDeveloper <dyandeveloper@hotmail.com>
2024-03-07 15:30:30 -0300

    App base

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // React
 import React from "react";
 // Local Import
-import "@/assets/styles/general.scss";
+import { Home, Projects, Feedback, Contact, Experience } from "@/pages";
+import { Navbar } from "@/components";
 
 function App() {
   return (
     <>
-    
+      <Navbar />
+      <Home />
+      <Experience />
+      <Projects />
+      <Feedback />
+      <Contact />
     </>
   )
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+// Import Local
+import "@/assets/styles/general.scss";
+
+// Export Pages
+export { default as Home } from './Home/Home';
+export { default as Projects } from './Projects/Projects';
+export { default as Feedback } from './Feedback/Feedback';
+export { default as Contact } from './Contact/Contact';
+export { default as Experience } from './Experience/Experience';
