95560a84980c91c829be78fd0bd29b18e8ca6095
DyanDeveloper <dyandeveloper@hotmail.com>
2024-03-07 15:11:22 -0300

    Ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// React
+import React from "react";
+// Local Import
+import "@/assets/styles/general.scss";
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// React Import
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Local Import
+import App from "@/App.jsx";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
