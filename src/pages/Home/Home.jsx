e4d634f42eabddc0bbcb8a78daee8263d2c15c2e
DyanDeveloper <dyandeveloper@hotmail.com>
2024-03-07 23:13:26 -0300

    Home View Features

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,11 +9,12 @@ const Home = () => {
           <h4 className="section-home__h4">Portafolio</h4>
           <h1 className="section-home__h1">Dylan Roldán</h1>
           <h2 className="section-home__h2">Ssr Full Stack Engineer | UI/UX Designer</h2>
+          <br />
           <p className="section-home__p">Creado con la intención de demostrar experiencia, categoría y a su vez que me conozcan un poco más.</p>
           <br />
           <div className="ctas-container">
-            <button type="button">Ver Experiencia</button>
-            <button type="button">Ver Proyectos</button>
+            <button type="button" className="home-cta">Experiencia</button>
+            <button type="button" className="home-cta">Contáctame</button>
           </div>
         </div>
       </section>
